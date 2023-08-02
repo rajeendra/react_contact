653a34572736a7eca10c4a1dca292929315d5e99
rajeendra <rajeendra.kanishka@gmail.com>
2023-08-02 14:18:46 +0530

    Test commit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,6 +5,7 @@ import Gateway from "../../route/Gateway";
 function Home() {
     // Login come after this, 
     // so values here would not get reset with this for each login 
+    //
     const [biz, setBiz] = useState({
       home:{signinOnce:false, appCount:0, rate:0},
       admin:{},
